1b3ba55bb32c5ee7227a5d36d19c47ad7c1ad24b
Ralph Seichter <github@seichter.de>
2025-02-12 14:09:10 +0100

    Improve tests

--- tests/test_stown.py
+++ tests/test_stown.py
@@ -16,6 +16,7 @@ You should have received a copy of the GNU General Public License along with
 stown. If not, see <https://www.gnu.org/licenses/>.
 """
 
+import json
 import os
 import shutil
 import stown.__main__ as stown
@@ -33,6 +34,11 @@ def truthy(x) -> bool:
     return x or x == 1 or x == "true" or x == "yes"
 
 
+def load_json(path):
+    with open(path, "rt") as f:
+        return json.load(f)
+
+
 class TestStown(unittest.TestCase):
     def setUp(self):
         os.chdir(os.path.dirname(__file__))
@@ -48,10 +54,16 @@ class TestStown(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def assert_json_equal(self, checkme: str, expected: str):
+        c = load_json(checkme)
+        x = load_json(expected)
+        self.assertEqual(c, x)
+
     def test_stow(self):
         self.assertEqual(stown.stown(stown.args.target, stown.args.source), 0)
         if not truthy(getenv("DISABLE_TREE")):
-            subprocess.run(["tree", "-aJ", "-o", "tree.json", stown.args.target])
+            subprocess.run(["tree", "-aJ", "-o", "tmp.json", stown.args.target])
+            self.assert_json_equal("tmp.json", "expected.json")
 
 
 if __name__ == "__main__":
